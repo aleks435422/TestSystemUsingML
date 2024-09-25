72d83b9714b8a706d9c4a62d34f9a51af51ee3b5
alekseev <alekseev-energy@yandex.ru>
2024-09-25 13:05:41 +0300

    Модель User.cs

---
+++ TestSystemUsingML/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSystemUsingML.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [ForeignKey("Role")]
+        public int? RoleId { get; set; }
+
+        public Role Role { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ TestSystemUsingML/ViewModels/AuthorizeViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TestSystemUsingML.Command;
+
+namespace TestSystemUsingML.ViewModels
+{
+    public class AuthorizeViewModel : BaseViewModel
+    {
+		public AuthorizeViewModel() 
+		{
+			QuitCommand = new RelayCommand(ExecuteQuitCommand, CanExecuteQuitCommand);
+		}
+		private string login;
+
+		public string Login
+		{
+			get { return login; }
+			set { login = value; OnPropertyChanged(); }
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set { password = value; OnPropertyChanged(); }
+		}
+
+		public ICommand QuitCommand { get; set; }
+
+		private void ExecuteQuitCommand(object parameter)
+		{
+			App.Current.Shutdown();
+		}
+
+		private bool CanExecuteQuitCommand(object parameter)
+		{
+			return true;
+		}
+
+	}
+}
